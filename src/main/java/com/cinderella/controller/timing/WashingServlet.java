47fd604351ae421f5d10c18e23e4d94cd3ba6839
lisz1012 <lisz1012@163.com>
2019-02-06 22:21:33 -0800

    Add the controller: WashingServlet and config related spring settings

---
+++ src/main/java/com/cinderella/controller/timing/WashingServlet.java
@@ -0,0 +1,56 @@
+package com.cinderella.controller.timing;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cinderella.dto.WashingInfo;
+import com.cinderella.dto.WashingInfo.WashingInfoBuilder;
+import com.cinderella.service.timing.WashingService;
+
+/**
+ * Servlet implementation class WashingController
+ */
+@Component
+@WebServlet("/washing")
+public class WashingServlet extends AbstractAutowiredHttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    @Autowired
+    private WashingService washingService;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WashingServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String userId = request.getParameter("userId");
+        String machineId = request.getParameter("machineId");
+        long washingDuration = Long.parseLong(request.getParameter("washingDuration"));
+        
+        WashingInfoBuilder builder = new WashingInfoBuilder();
+        builder.withUserId(userId)
+               .withMachineId(machineId)
+               .withWashingDuration(washingDuration);
+        WashingInfo washingInfo = builder.build();
+        
+        System.out.println(washingService);
+        washingService.process(washingInfo);
+    }
+
+}
---
+++ src/main/java/com/cinderella/service/timing/WashingService.java
@@ -0,0 +1,7 @@
+package com.cinderella.service.timing;
+
+import com.cinderella.dto.WashingInfo;
+
+public interface WashingService {
+    void process(WashingInfo washingInfo);
+}
