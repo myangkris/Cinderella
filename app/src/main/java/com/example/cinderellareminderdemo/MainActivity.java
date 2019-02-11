8b72072c98381de5d96e459614270a991ed9e8ec
lisz1012 <lisz1012@163.com>
2019-02-10 22:36:32 -0800

    Config manual push

--- app/src/main/java/com/example/cinderellareminderdemo/MainActivity.java
+++ app/src/main/java/com/example/cinderellareminderdemo/MainActivity.java
@@ -3,11 +3,28 @@ package com.example.cinderellareminderdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.jpush.android.api.JPushInterface;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(getApplicationContext());
+        System.out.println(JPushInterface.getRegistrationID(getApplicationContext()));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        JPushInterface.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        JPushInterface.onPause(this);
     }
 }
