bb98c0db6f8254312bd682f0c3557907037fb2aa
myangkris <myangkris@gmail.com>
2019-02-13 15:56:53 -0500

    machine status check v1.0

--- src/components/Content.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export class Content extends React.Component {
-    render() {
-        return(
-            <h1>This is Content</h1>
-        )
-    }
-}
--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import {Header} from './Header'
 import {Footer} from './Footer'
-import {Content} from './Content'
+import {MachineStatus} from './MachineStatus'
 
 export class Reserve extends React.Component {
     render() {
         return(
             <div>
                 <Header/>
-                <Content/>
+                <section className="status-wrapper">
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                    <MachineStatus/>
+                </section>
                 <Footer/>
             </div>
         )
