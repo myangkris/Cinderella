f5d22aafc4a13ba5361afc1182e7c80093a44e82
yunhaoguo <929394263@qq.com>
2019-02-18 00:03:41 -0500

    add login page

---
+++ app/src/main/java/com/yunhaoguo/cinderella/login/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.yunhaoguo.cinderella.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.yunhaoguo.cinderella.MainActivity;
+import com.yunhaoguo.cinderella.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        Button loginBtn = findViewById(R.id.btn_login);
+        loginBtn.setOnClickListener(this);
+        Button signupBtn = findViewById(R.id.btn_signup);
+        signupBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.btn_signup:
+                break;
+        }
+    }
+}
