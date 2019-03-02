be7a471f77081f8c049754cb59e88fac73809996
Liang200 <827865721@qq.com>
2019-03-02 01:59:28 -0500

    pay login register endpoint

---
+++ src/main/java/com/cinderella/service/account/Account.java
@@ -0,0 +1,38 @@
+package com.cinderella.service.account;
+
+import org.json.JSONObject;
+
+import com.cinderella.dao.DBConnection;
+import com.cinderella.dao.DBConnectionFactory;
+import com.cinderella.entity.User;
+
+public abstract class Account {
+	
+	// set name 
+	// set address 
+	// set email
+	// set phoneNumber
+	// set password
+	public static boolean logIn(String userName, String password) {
+		DBConnection connection = DBConnectionFactory.getConnection();
+		boolean logined = connection.verifyLogin(userName, password);
+		connection.close();
+		return logined;
+	};
+	
+	public static boolean register(User user) {
+		DBConnection connection = DBConnectionFactory.getConnection();
+		if (connection.findUserByUsername(user.getName()) == null) {
+			boolean registered = connection.addUser(user);
+			connection.close();
+			System.out.println("add already done");
+			return registered;
+		} 
+		return false;
+	};
+	
+	public abstract boolean updateProfile(User user);
+	
+	public abstract JSONObject getProfile();
+	
+}
